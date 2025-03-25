84fbf2404e8f23fe42d4116a349b74c1619e9d04
태규 <123963462+EOTAEGYU@users.noreply.github.com>
2025-03-25 20:49:13 +0900

    init: 프로젝트 생성

---
+++ upbit-streaming/src/main/java/org/example/upbitstreaming/UpbitStreamingApplication.java
@@ -0,0 +1,13 @@
+package org.example.upbitstreaming;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UpbitStreamingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UpbitStreamingApplication.class, args);
+    }
+
+}
