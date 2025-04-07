2a2d372e40d900d8ea1213005a2b45f0c05c30c5
unknown <kodms08@naver.com>
2025-04-07 23:25:54 +0900

    ✏️fix: data.json 파일 경로 수정

--- upbit-streaming/src/main/java/org/stofka/upbitstreaming/OrderbookWebSocketService.java
+++ upbit-streaming/src/main/java/org/stofka/upbitstreaming/OrderbookWebSocketService.java
@@ -5,11 +5,13 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import okhttp3.*;
 import okio.ByteString;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.PostConstruct;
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
@@ -74,14 +76,18 @@ public class OrderbookWebSocketService {
     }
 
     private void loadMarkets() throws IOException {
-        File file = Paths.get(dataFilePath).toFile();
-        JsonNode jsonNode = objectMapper.readTree(file);
+        String classpathLocation = dataFilePath.replace("classpath:", "");
+        ClassPathResource resource = new ClassPathResource(classpathLocation);
 
-        for (JsonNode node : jsonNode) {
-            String market = node.get("market").asText();
-            markets.add(market);
+        try (InputStream is = resource.getInputStream()) {
+            JsonNode jsonNode = objectMapper.readTree(is);
+
+            for (JsonNode node : jsonNode) {
+                String market = node.get("market").asText();
+                markets.add(market);
+            }
+            System.out.println("📌 KRW 종목 로드 완료: " + markets.size() + "개");
         }
-        System.out.println("📌 KRW 종목 로드 완료: " + markets.size() + "개");
     }
 
     private static void sendSubscriptionMessage(WebSocket webSocket) throws IOException {
