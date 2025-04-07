1bc460fbe8b302e336d3fd930184d0afbafe7a40
unknown <kodms08@naver.com>
2025-04-07 23:59:35 +0900

    ✨feat: default controller 생성

---
+++ upbit-streaming/src/main/java/org/stofka/upbitstreaming/controller/CheckController.java
@@ -0,0 +1,15 @@
+package org.stofka.upbitstreaming.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CheckController {
+
+    @GetMapping("/")
+    public ResponseEntity<String> health() {
+        return ResponseEntity.ok("OK");
+    }
+
+}
